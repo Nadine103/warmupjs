5d35edcd0f12eaaba837655b4b4b878f0793441b
Nadine101 <nadinet026@gmail.com>
2022-07-12 16:33:07 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let my_city = {
+    my_city:(`Brampton`),
+    population: 700000,
+    is_capital:true
+}
+if(my_city[`population`] >= 700000) {
+    console.log(`The great city:${my_city[`name`]},`)
+}
